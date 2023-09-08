1727e2a0a3e1c7e96a4dd771ac9d9a33bb6e90a5
Rafael Meireles <rafael.meireles.prestserv@petrobras.com.br>
2023-09-07 21:35:47 -0300

    update

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Projeto em JavaScript para exibir os usuário do github
 Iniciando o Yarn
 yarn init  
 Informe os valores de configuração do projeto
-
+dependências de DEV
 yarn add @babel/cli -D
 yarn add @babel/core -D 
 yarn add @babel/preset-env -D 
@@ -15,7 +15,10 @@ yarn add babel-loader -D
 yarn add webpack -D 
 yarn add webpack-cli -D 
 yarn add webpack-dev-server -D
+
+dependências de PROD
 yarn add @babel/polyfill
+yarn add axios
 ```
 
 1. Crie o arquivo .babelrc, webpack.config.js
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-class APP{
+import api from './api';
+
+class App{
     //Construtor
     constructor(){
-        // Lista de repositórios
-        this.repositorios = [];
+        // Carrega os dados do localStorage
+
+        this.repositorios = JSON.parse(localStorage.getItem('repositorios')) || [];
 
         // Form
         this.formulario = document.querySelector('form');
@@ -12,26 +15,83 @@ class APP{
 
         // Método para registrar os eventos do form
         this.registrarEventos();
+        
+        // Chama salvarRepositoriosNoLocalStorage para garantir que os dados sejam salvos
+        this.salvarRepositoriosNoLocalStorage();
+
+        // Busca os dados já salvos
+        this.renderizarTela();
     }
+
+    salvarRepositoriosNoLocalStorage() {
+        localStorage.setItem('repositorios', JSON.stringify(this.repositorios));
+    };
 
     registrarEventos(){
         this.formulario.onsubmit = evento => this.adicionarRepositorio(evento)
-    }
+    };
 
-    adicionarRepositorio(evento){
+    async adicionarRepositorio(evento){
         // Evita que o formulário recarregue a página
         evento.preventDefault();
 
-        // Adiciona o repositório na Lista
-        this.repositorios.push({
-            nome: 'Nerd Fonts',
-            descricao: 'Iconic font aggregator, collection, and patcher.',
-            avatar_url: 'https:/avatars0.githubusercontent.com/u/8083459?v=4',
-            link: 'https://github.com/ryanoasis/nerd-fonts'
-        });
+        // Recuperar o valor do input
+        let input = this.formulario.querySelector('input[id=repositorio]').value;
 
-        // Renderizar a tela
-        this.renderizarTela()
+        // Se o input vier vazio... sai da app
+        if(input.length === 0){
+            return; // return sempre sai da função
+        }
+
+        // Ativa o carregamento
+        this.apresentarBuscando();
+
+        try{
+            let response = await api.get(`/repos/${input}`);
+
+            // console.log(response);
+            let { name, description, html_url, owner: {avatar_url}} = response.data;
+    
+            // Adiciona o repositório na Lista
+            this.repositorios.push({
+                nome: name,
+                descricao: description,
+                avatar_url,
+                link: html_url,
+            });
+
+            // Renderizar a tela
+            this.renderizarTela()
+
+            // Salva os repositórios no localStorage
+            this.salvarRepositoriosNoLocalStorage();
+        }catch(erro){
+            // Limpa Busca, vamos remover a mensagem de busca e deixar apenas a de erro
+            this.lista.removeChild(document.querySelector('.list-group-item-warning'));
+
+            // Limpar erro existente
+            let er = this.lista.querySelector('.list-group-item-danger');
+            if(er !== null){
+                this.lista.removeChild(er);
+            }
+
+            //<li>
+            let li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item list-group-item-danger');
+            let txtErro = document.createTextNode(`O repositório ${input} não existe.`);
+            li.appendChild(txtErro);
+            this.lista.appendChild(li);
+        }
+
+    }
+
+    apresentarBuscando(){
+        //<li>
+        let li = document.createElement('li');
+        li.setAttribute('class', 'list-group-item list-group-item-warning');
+        let txtBuscando = document.createTextNode(`Aguarde, buscando o repositório...`);
+        li.appendChild(txtBuscando);
+        this.lista.appendChild(li);
     }
 
     renderizarTela(){
@@ -39,7 +99,7 @@ class APP{
         this.lista.innerHTML = '';
 
         // Percorrer toda a lista de repositórios e criar os elementos
-        this.repositorios.forEach(repositorio => {
+        this.repositorios.forEach((repositorio, index) => {
             //<li>
             let li = document.createElement('li');
             li.setAttribute('class', 'list-group-item list-group-item-action');
@@ -61,13 +121,30 @@ class APP{
             p.appendChild(txtDescricao)
             li.appendChild(p);
 
-            //<a>
+            //<a> Acessar
             let a = document.createElement('a');
             a.setAttribute('target', '_blank');
             a.setAttribute('href', repositorio.link);
             let txtA = document.createTextNode('Acessar');
             a.appendChild(txtA);
             li.appendChild(a);
+
+            // Adicione um espaço em branco entre "Acessar" e "Remover"
+            let espacoEmBranco = document.createTextNode(' '); // Cria um nó de texto contendo um espaço em branco
+            li.appendChild(espacoEmBranco);
+
+            //<a> remover
+            let removerLink = document.createElement('a');
+            removerLink.setAttribute('href', '#');
+            let txtRemover = document.createTextNode('Remover');
+            removerLink.appendChild(txtRemover);
+            li.appendChild(removerLink);
+
+            // Adicione um evento de clique para remover o repositório
+            removerLink.addEventListener('click', (evento) => {
+                evento.preventDefault();
+                this.removerRepositorio(index);
+            });
 
             // Adicionar <li> como filho da ul
             this.lista.appendChild(li);
@@ -78,7 +155,19 @@ class APP{
             // Adiciona o foco no input
             this.formulario.querySelector('input[id=repositorio]').focus();
         });
-    }
+    };
+
+    // Função para remover um repositório da lista
+    removerRepositorio(index) {
+    // Remove o repositório da lista
+    this.repositorios.splice(index, 1);
+
+    // Renderiza a tela novamente após a remoção
+    this.renderizarTela();
+
+    // Atualiza o localStorage após a remoção
+    this.salvarRepositoriosNoLocalStorage();
+  }
 }
 
-new APP();
+new App();
