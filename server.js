435eef9d364ef47ae92aeb2f37ff45d28b74e0e8
Rafael Meireles <rafael.meireles.prestserv@petrobras.com.br>
2023-09-20 20:22:15 -0300

    test fix

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const http = require('http');
-const app = require('./app')
+const { createServer } = require('http');
+const App = require('./src/App'); // Certifique-se de que o caminho e o nome do arquivo estão corretos
 
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+const port = process.env.PORT || 8080;
+const server = createServer(App);
 
-// const server = http.createServer((req, res) => {
-//   // Lida com as solicitações HTTP aqui
-//   res.end('Hello, world!');
-// });
-
-// const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
