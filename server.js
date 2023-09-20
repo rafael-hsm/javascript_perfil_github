23ce0dada409c24bd4d3fbd547dc03f186da3fa7
Rafael Meireles <rafael.meireles.prestserv@petrobras.com.br>
2023-09-20 19:33:23 -0300

    update

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require('http');
+const app = require('./app')
 
-const server = http.createServer((req, res) => {
-  // Lida com as solicitações HTTP aqui
-  res.end('Hello, world!');
-});
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 
-const port = process.env.PORT || 10000;
+// const server = http.createServer((req, res) => {
+//   // Lida com as solicitações HTTP aqui
+//   res.end('Hello, world!');
+// });
+
+// const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
